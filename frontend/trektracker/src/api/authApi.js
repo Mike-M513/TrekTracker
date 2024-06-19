acac4815ededd2359eb6bc942d87cb3b5257450c
kborgos <katya.borgos@gmail.com>
2024-06-18 20:06:56 -0500

    Added visited parks data (cards) to tracker page. Populating using dummy data, API integration pending.

---
+++ frontend/trektracker/src/api/authApi.js
@@ -0,0 +1,24 @@
+async function basicFetch(url, payload) {
+    const res = await fetch(url, payload)
+    const body = await res.json()
+    return body
+  }
+
+  export async function getVisitedParks(token, user) {
+    // uncomment and edit with correct route when ready to do API call
+    // const payload = {
+    //   method: "GET",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     "Authorization": `Token ${token}`
+    //   }  }
+    // const body = await basicFetch(`http://localhost:8000/visits/user/${user}`, payload)
+
+    // dummmy data
+    const body = [{"name": "Yellowstone", "visit_date": "05-25-21", "activities": "Hiking, swimming"}, 
+        {"name": "Yosemite", "visit_date": "02-11-18", "activities": "Hiking"}, 
+        {"name": "Grand Canyon", "visit_date": "10-05-19", "activities": "Kayaking"},
+        {"name": "Great Dunes", "visit_date": "04-18-17", "activities": "Hiking"}]
+
+    return body
+  }
