230b13d250f9f98804528ab37d86b77771f33a32
kborgos <katya.borgos@gmail.com>
2024-06-27 17:41:59 -0500

    Minor changes, just updating typo in name of the app.

--- frontend/trektracker/src/App.jsx
+++ frontend/trektracker/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
-import Trakker from './pages/Trakker';
+import Tracker from './pages/Tracker';
 import Navigation from './components/Navigation'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
@@ -12,7 +12,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/trakker" element={<Trakker />} />
+        <Route path="/tracker" element={<Tracker />} />
       </Routes>
     </BrowserRouter>
   )
