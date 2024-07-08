c996caf413e553100172a25b29396ddf2ced4c82
mike-meier <mikemeierdev@gmail.com>
2024-07-08 19:02:06 -0400

    added break to register page

--- backend/park/models.py
+++ backend/park/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Park(models.Model):
     park_name = models.CharField(max_length=255, unique=True)
     park_code = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.park_name
